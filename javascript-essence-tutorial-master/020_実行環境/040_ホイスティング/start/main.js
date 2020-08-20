3f20e75bf3b0f8b198e6653f18c7b9ab7a67b1e6
TY's MacBook Pro <teraokayuki@TYsnoMacBook-Pro.local>
2020-08-20 19:00:30 +0900

    add new file

---
+++ javascript-essence-tutorial-master/020_実行環境/040_ホイスティング/start/main.js
@@ -0,0 +1,14 @@
+a();
+
+function a() {
+    
+    let c = 1;
+    d();
+    function d() {
+        console.log('d is called')
+    }
+    console.log('a is called');
+}
+
+const b = 0;
+console.log(b);
