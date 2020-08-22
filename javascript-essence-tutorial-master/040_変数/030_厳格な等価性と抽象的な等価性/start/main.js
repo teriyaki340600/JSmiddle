a8bc5f6b65e92d550e9cab6ee19a1fa0540201e1
Yuki Teraoka <tvyq1022@gmail.com>
2020-08-22 13:36:26 +0900

    day3

---
+++ javascript-essence-tutorial-master/040_変数/030_厳格な等価性と抽象的な等価性/start/main.js
@@ -0,0 +1,17 @@
+function printEquality(a, b) {
+    console.log(a === b)
+    console.log(a == b)
+}
+
+
+let a = '1';
+let b = 1;
+let c = true;
+
+
+// printEquality(b, c)
+
+let e = "";
+let f = 0;
+
+printEquality(e, f);
