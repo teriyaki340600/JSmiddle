ba6fefe2d546d5ee82368a12d27de85405efa2a6
Yuki Teraoka <tvyq1022@gmail.com>
2020-10-05 21:51:38 +0900

    20201005

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/110_super/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/110_super/start/main.js
@@ -27,17 +27,25 @@ class Japanese extends Person {
 
 const taro = new Japanese('Taro', 23, 'Male');
 console.log(taro);
-taro.bye();
+taro.hello();
 
-// const american = {
-//     hello() {
-//         console.log('hello ' + this.name);
-//     }
+const american = {
+    hello() {
+        console.log('hello ' + this.name);
+    }
+}
+
+const bob = {
+    name: 'Bob',
+    hello() {
+        super.hello();
+        // console.log('hello ' + this.name);
+    }
+}
+// bob.bye = function() {
+//     super.hello();
 // }
 
-// const bob = {
-//     name: 'Bob',
-//     hello() {
-//         super.hello();
-//     }
-// }
+Object.setPrototypeOf(bob, american);
+
+bob.hello();
